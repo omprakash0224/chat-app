780270a5fb5c04abc939e34f21e241860083906e
Om Prakash <omprakash.snps@gmail.com>
2026-01-24 16:45:02 +0530

    Implement user signup and database integration
    Added MongoDB connection logic, user and message models, and utility for JWT token generation. Enhanced the signup controller to handle user registration with validation, password hashing, and token creation. Updated server initialization to load environment variables, connect to the database, and parse JSON requests.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("API is running...");
@@ -12,4 +18,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
